2ff7b87f623e7866e18d7634f813762f7def1c9a
Easton Kurth <145180001+Easturzaa@users.noreply.github.com>
2024-09-03 17:27:32 -0500

    Portfolio
    :)

---
+++ script.js
@@ -0,0 +1,10 @@
+/* LOADING SCREEN */
+
+window.addEventListener("load", function () {
+  setTimeout(function () {
+    document.querySelector("#loader-wrapper").style.opacity = 0;
+    setTimeout(function () {
+      document.querySelector("#loader-wrapper").style.display = "none";
+    }, 1000);
+  }, 4000);
+});
